7ef1e68c5ff6a5f706c1eaab1510a0918295dfdf
Ramon Akhmetshin <eramonk@gmail.com>
2019-08-29 15:01:48 +0300

    wip

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,8 @@ class User < ApplicationRecord
 
   def generate_jwt
     JWT.encode({ id: id,
-                exp: 60.days.from_now.to_i },
-               Rails.application.secrets.secret_key_base)
+                exp:j 60.days.from_now.to_i },
+              #  Rails.application.secrets.secret_key_base)
+               ENV['JWT_SECRET_KEY'])
   end
 end
