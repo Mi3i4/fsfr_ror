685dd5c95b936e880b2e8af05ff8452e52b1726c
Ramon Akhmetshin <eramonk@gmail.com>
2019-09-03 05:47:26 +0300

    исправил баг с в ApplicationController секретным ключем

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,7 +35,7 @@ class ApplicationController < ActionController::API
     if request.headers["Authorization"].present?
       authenticate_or_request_with_http_token do |token|
         begin
-          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+          jwt_payload = JWT.decode(token, ENV['JWT_SECRET_KEY']).first
 
           @current_user_id = jwt_payload["id"]
         rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
 
   def generate_jwt
     JWT.encode({ id: id,
-                exp:j 60.days.from_now.to_i },
+                exp: 60.days.from_now.to_i },
               #  Rails.application.secrets.secret_key_base)
                ENV['JWT_SECRET_KEY'])
   end
